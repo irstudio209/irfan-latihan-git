bf6af5e783beea525ac9f54d66567680dec10687
irfanrmdni <irfanrmdni209@gmail.com>
2020-10-15 16:23:27 +0700

    menambah output pada branch latihan 3

--- lathan3.cpp
+++ lathan3.cpp
@@ -6,5 +6,7 @@ int main(int argc, char const *argv[])
 {
     cout << "Latihan GIT" << endl;
     cout << "ini adalah file Branch latihan 3" << endl;
+
+    cout << "hello dunia" << endl;
     return 0;
 }
--- latihan1.cpp
+++ latihan1.cpp
@@ -7,7 +7,6 @@ int main(int argc, char const *argv[])
     cout << "Latihan GIT" << endl;
     cout << "ini adalah file 1" << endl;
 
-    cout << "Hello word" << endl;
     cout << "hello kalian" << endl;
     return 0;
 }
