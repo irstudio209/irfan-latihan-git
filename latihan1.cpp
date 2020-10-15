98748ca685c2e6b1b6f055803736c5dfa342d76c
IRStudio <72926037+irstudio209@users.noreply.github.com>
2020-10-15 22:33:35 +0700

    mengubah output pada latihan1.cpp

--- latihan1.cpp
+++ latihan1.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout << "Latihan GIT" << endl;
-    cout << "ini adalah file 1" << endl;
+    cout << "ini adalah file sanguan" << endl;
     return 0;
 }
