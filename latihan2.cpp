892edca718250f4b98e1f49ffca1f11b0e9e5c4e
irfanrmdni <irfanrmdni209@gmail.com>
2020-10-15 16:20:27 +0700

    mengubah file output pada latihan 2.cpp

--- latihan2.cpp
+++ latihan2.cpp
@@ -7,6 +7,6 @@ int main(int argc, char const *argv[])
     cout << "Latihan GIT" << endl;
     cout << "Ini adalah file 2" << endl;
 
-    cout << "Hello word" << endl;
+    cout << "selamat datang" << endl;
     return 0;
 }
