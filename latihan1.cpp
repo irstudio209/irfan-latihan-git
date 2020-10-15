22163e047457f0df617e65f69085c1071c77c8f9
irfanrmdni <irfanrmdni209@gmail.com>
2020-10-15 16:09:13 +0700

    membuat file latihan 1.cpp

---
+++ latihan1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Latihan GIT" << endl;
+    cout << "ini adalah file 1" << endl;
+    return 0;
+}
